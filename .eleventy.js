00a1762fa2edd4c5777ace3cfcef0d51c8af5a75
Jochen Stierberger <jochen@stierberger.com>
2024-04-07 18:11:15 -0500

    feat: add RSS plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(pluginRss);
 
   // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
