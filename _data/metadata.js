e7981e65a59829866d79d5343d08591fe52cb884
Jochen Stierberger <jochen@stierberger.com>
2024-04-08 18:53:26 -0500

    feat: add metadata.js

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Jochen Stierberger - Personal Website",
+  base_url: "jochen.fyi",
+  url: "https://jochen.fyi/",
+  language: "en",
+  description: "I am writing about my experiences as a naval navel-gazer.",
+  author: {
+    name: "Your Name Here",
+    email: "youremailaddress@example.com",
+    url: "https://example.com/about-me/"
+  }
+}
