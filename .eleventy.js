afeb29597bdfeb9e779f308a1f5dfb93921287ac
Jochen Stierberger <jochen@stierberger.com>
2024-04-10 18:26:03 -0500

    fix: remove prism and add additional filter

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,6 @@ module.exports = function(eleventyConfig) {
   // For example, `./public/css/` ends up in `_site/css/`
   eleventyConfig.addPassthroughCopy({
     "./public/": "/",
-    "./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
   });
 
   // Official plugins
@@ -27,6 +26,15 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
   });
 
+  // Return all the tags used in a collection
+  eleventyConfig.addFilter("getAllTags", collection => {
+    let tagSet = new Set();
+    for (let item of collection) {
+      (item.data.tags || []).forEach(tag => tagSet.add(tag));
+    }
+    return Array.from(tagSet);
+  });
+
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
     return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
@@ -40,22 +48,36 @@ module.exports = function(eleventyConfig) {
     return (new Date()).toDateString();
   })
 
+  eleventyConfig.addFilter("readableTimestamp", function(dateVal, locale = "en-us") {
+    var theDate = new Date(dateVal);
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour12: true,
+      hour: '2-digit',
+      minute: '2-digit'
+    };
+    return theDate.toLocaleString(locale, options);
+  });
+
   // Return the smallest number argument
-	eleventyConfig.addFilter("min", (...numbers) => {
-		return Math.min.apply(null, numbers);
-	});
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
 
-	// Get the first `n` elements of a collection.
-	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
-			return [];
-		}
-		if( n < 0 ) {
-			return array.slice(n);
-		}
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
 
-		return array.slice(0, n);
-	});
+    return array.slice(0, n);
+  });
 
   return {
     templateFormats: [
