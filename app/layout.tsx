1af77be8711a805a952adb1f79a4b029c1fb2283
strbrgr <jocsti@web.de>
2025-07-30 18:07:39 -0500

    use manrope as font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavLink from "./components/nav-link";
+import { Manrope } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Jochen Stierberger",
@@ -13,13 +14,18 @@ const headerLinks: { href: string; title: string }[] = [
   // { href: "/posts", title: "posts" },
 ];
 
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
+    <html lang="en" className={manrope.className}>
       <body className="antialiased min-h-screen flex justify-center overflow-y-scroll px-4">
         <div className="max-w-2xl flex flex-col min-h-screen w-full">
           <header className="mt-4">
