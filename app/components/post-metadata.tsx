61843a201c43edba31bb58a170fa0fa97b6bb434
strbrgr <jocsti@web.de>
2025-12-12 07:22:02 -0600

    Remove updated information from Post Metadata

--- app/components/post-metadata.tsx
+++ app/components/post-metadata.tsx
@@ -1,24 +1,14 @@
-import { getFileLastModifiedDate } from "@/utils/getLastModifiedDate";
-
 type Props = {
   created: string;
-  page: string;
 };
 
-export default function PostMetadata({ page, created }: Props) {
-  const dateString = getFileLastModifiedDate(page);
+export default function PostMetadata({ created }: Props) {
 
   return (
     <div className="my-2 mb-6 text-sm text-gray-400 flex flex-col">
       <span>
         Created:{" "}
         <time dateTime={created}>{new Date(created).toLocaleDateString()}</time>
-      </span>
-      <span>
-        Last updated:{" "}
-        <time dateTime={dateString}>
-          {new Date(dateString).toLocaleDateString()}
-        </time>
       </span>
     </div>
   );
